93e9c304f13a361aa591ef276c6306a8de4a922e
Emin Muhammadi <emin.muhammadi@nokia.com>
2022-12-16 18:11:47 +0100

    tw update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: "class",
   theme: {
+    ...defaultTheme,
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    container: {
+      padding: '1rem',
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-children'),
+  ],
 }
